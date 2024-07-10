322b0cdd9c43b0462d8bfc1ef4a2660d06116e4b
GurnoorOberoi <gurnoor9965@gmail.com>
2024-07-10 23:17:00 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+//import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract myToken is ERC20 {
+    address owner;
+
+    constructor(uint _initialSupply) ERC20("Gurnoor", "GUR") {
+        owner = msg.sender;
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function mint(address to, uint amount) public {
+        require(msg.sender == owner, "Not Authorized");
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+}
